35e04e173810b48deeafdcfa2b43738f43ba7632
Kanishk Pachauri <krishna03012003@icloud.com>
2022-10-14 05:57:01 +0530

    Added Lottie Assets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,25 +13,29 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <ul className='flex hidden'>
+        <RiOpenSourceFill size={60} color='green'/>
+        <h1 className='w-full text-3xl font-bold text-[#0DFF1C] m-4'> FOSSCU</h1>
+        <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>About</li>
             <li className='p-4'>Team</li>
             <li className='p-4'>Projects</li>
             <li className='p-4'>Contact</li>
         </ul>
-        <div onClick={handleNav}>
-            {!nav ? <AiOutlineClose size={20}/> :<AiOutlineMenu size={20} />}
+        <div onClick={handleNav} className='block md:hidden'>
+            {nav ? <AiOutlineClose size={20}/> :<AiOutlineMenu size={20} />}
         </div>
-        <div className='fixed left-0 top-0 w-[60%] h-full border-r border-r-grey-900 bg-[#000300]'>
-        <RiOpenSourceFill size={57} color='green'/>
-        <h1 className='w-full text-3xl font-bold text-[#0DFF1C] m-4'> FOSSCU</h1> 
-            <ul className='uppercase p-4'>
-                <li className='p-4 border-b border-gray-600'>Home</li>
-                <li className='p-4 border-b border-gray-600'>About</li>
-                <li className='p-4 border-b border-gray-600'>Team</li>
-                <li className='p-4 border-b border-gray-600'>Projects</li>
-                <li className='p-4 border-b border-gray-600'>Contact</li>
+        <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-grey-900 bg-[#000300] ease-in-out duration-500 ' : 'fixed left-[-100%]'}>
+            <center>
+                <RiOpenSourceFill size={65} color='green'/>
+                <h1 className='w-full text-3xl font-bold text-[#0DFF1C] m-4'> FOSSCU</h1> 
+            </center>
+                <ul className='uppercase p-4'>
+                    <li className='p-4 border-b border-gray-600'>Home</li>
+                    <li className='p-4 border-b border-gray-600'>About</li>
+                    <li className='p-4 border-b border-gray-600'>Team</li>
+                    <li className='p-4 border-b border-gray-600'>Projects</li>
+                    <li className='p-4 border-b border-gray-600'>Contact</li>
             </ul>
         </div>
     </div>
