b056324f3c42b781c91c71a65f11c14c421321b8
Kanishk Pachauri <krishna03012003@icloud.com>
2022-10-14 05:56:07 +0530

    Added new routes 🚏

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
