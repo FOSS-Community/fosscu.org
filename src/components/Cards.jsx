4d5a305f15f515ccc7b9bdcb9ac01306aa36da5b
Kanishk Pachauri <krishna03012003@icloud.com>
2022-10-17 08:46:05 +0530

    Cards Added 🃏

---
+++ src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='w-full py-[10rem] px-4 bg-white'>
+        <div>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Cards
