288604f346d77b7d313ef527ce87d52c71a54b1f
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-11-23 00:54:22 +0530

    team page upadate

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -8,14 +8,15 @@ import Mayank from '../assets/img/team/mayank.jpeg'
 import Aditya_Bansal from '../assets/img/team/aditya_bansal.jpeg'
 import Yash_Varshney from '../assets/img/team/yash_varshney.jpeg'
 import Aryan_Kaushik from '../assets/img/team/aryan_kaushik.jpeg'
-
-
-
+import Srijan_Srivastava from '../assets/img/team/Srijan_Srivastava.jpg'
+import Tejaswita_Agarwal from '../assets/img/team/Tejaswita_Agarwal.jpg'
+import Devanshu_Saxena from '../assets/img/team/Devanshu_Saxena.jpg'
 
 import {
   FaGithubSquare,
   FaInstagram,
   FaTwitterSquare,
+  FaLink
 } from 'react-icons/fa';
 
 const Team = () => {
@@ -31,10 +32,8 @@ const Team = () => {
                 <img className='w-20 mx-auto mt-[-3rem] bg-white profile_pic' src={Akshat_Sharma} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8 text-white'>AKSHAT SHARMA</h2>
                 <p className='text-center text-1xl font-bold text-white'>President and Web3 Developer</p>
-                <div className='flex justify-between mx-auto md:w-[50%] my-6 px-6 py-3'>
-                    <a href="https://www.instagram.com/fosscu/"> <FaInstagram size={30} color='white'/> </a>
-                    <a href="https://twitter.com/fosscuk"> <FaTwitterSquare size={30} color='white'/> </a>
-                    <a href="https://github.com/FOSS-Community/"> <FaGithubSquare size={30} color='white'/> </a>
+                <div className='flex justify-center mx-auto md:w-[50%] my-6 px-6 py-3'>
+                    <a href=""> <FaLink  size={30} color='white'/> </a>
                 </div>
             </div>
 
@@ -42,55 +41,71 @@ const Team = () => {
                 <img className='w-20 mx-auto mt-[-3rem] bg-white profile_pic' src={Kansihk_Pachauri} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8 text-white'>KANISHK PACHAURI</h2>
                 <p className='text-center text-1xl font-bold text-white'>Community Manager and Backend Developer</p>
-                <div className='flex justify-between mx-auto md:w-[50%] my-6 px-6 py-3'>
-                    <a href="https://www.instagram.com/fosscu/"> <FaInstagram size={30} color='white' /> </a>
-                    <a href="https://twitter.com/fosscuk"> <FaTwitterSquare size={30} color='white' /> </a>
-                    <a href="https://github.com/FOSS-Community/"> <FaGithubSquare size={30} color='white' /> </a>
+                <div className='flex justify-center mx-auto md:w-[50%] my-6 px-6 py-3'>
+                <a href=""> <FaLink  size={30} color='white'/> </a>
+                </div>
+            </div>
+
+            <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                <img className='w-20 mx-auto mt-[-3rem] bg-white profile_pic' src={Tejaswita_Agarwal} alt="/" />
+                <h2 className='text-2xl font-bold text-center py-8 text-white'>Tejaswita Agarwal</h2>
+                <p className='text-center text-1xl font-bold text-white'>T.B.A</p>
+                <div className='flex justify-center mx-auto md:w-[50%] my-6 px-6 py-3'>
+                <a href=""> <FaLink  size={30} color='white'/> </a>
                 </div>
             </div>
 
             <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
                 <img className='w-20 mx-auto mt-[-3rem] bg-white profile_pic' src={Aryan_Kaushik} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8 text-white'>ARYAN KAUSHIK</h2>
-                <p className='text-center text-1xl font-bold text-white'>T.B.A</p>
-                <div className='flex justify-between mx-auto md:w-[50%] my-6 px-6 py-3'>
-                    <a href="https://www.instagram.com/aryan__kaushik_/"> <FaInstagram size={30} color='white'/> </a>
-                    <a href="https://twitter.com/Aryan_Ka1"> <FaTwitterSquare size={30} color='white' /> </a>
-                    <a href="https://github.com/Aryan20/"> <FaGithubSquare size={30} color='white' /> </a>
+                <p className='text-center text-1xl font-bold text-white'>Volunteer in Linux Software Development</p>
+                <div className='flex justify-center mx-auto md:w-[50%] my-6 px-6 py-3'>
+                <a href=""> <FaLink  size={30} color='white'/> </a>
                 </div>
             </div>
 
             <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
                 <img className='w-20 mx-auto mt-[-3rem] bg-white profile_pic' src={Aditya_Bansal} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8 text-white'>ADITYA BANSAL</h2>
-                <p className='text-center text-1xl font-bold text-white'>Content and Discord Lead</p>
-                <div className='flex justify-between mx-auto md:w-[50%] my-6 px-6 py-3'>
-                    <a href="https://www.instagram.com/fosscu/"> <FaInstagram size={30} color='white' /> </a>
-                    <a href="https://twitter.com/fosscuk"> <FaTwitterSquare size={30} color='white' /> </a>
-                    <a href="https://github.com/FOSS-Community/"> <FaGithubSquare size={30} color='white' /> </a>
+                <p className='text-center text-1xl font-bold text-white'>Volunteer in Social Media</p>
+                <div className='flex justify-center mx-auto md:w-[50%] my-6 px-6 py-3'>
+                <a href="https://linktr.ee/adityabansal2310 "> <FaLink  size={30} color='white'/> </a>
                 </div>
             </div>
 
 
-            <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-                <img className='w-20 mx-auto mt-[-3rem] bg-white profile_pic' src={Yash_Varshney} alt="/" />
-                <h2 className='text-2xl font-bold text-center py-8 text-white'>YASH VARSHNEY</h2>
-                <p className='text-center text-1xl font-bold text-white'>T.B.A</p>
-                <div className='flex justify-between mx-auto md:w-[50%] my-6 px-6 py-3'>
-                    <a href="https://www.instagram.com/fosscu/"> <FaInstagram size={30} color='white' /> </a>
-                    <a href="https://twitter.com/fosscuk"> <FaTwitterSquare size={30}  color='white'/> </a>
-                    <a href="https://github.com/FOSS-Community/"> <FaGithubSquare size={30} color='white' /> </a>
-                </div>
-            </div>
             
             <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
                 <img className='w-20 mx-auto mt-[-3rem] bg-white profile_pic' src={Mayank} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8  text-white'>MAYANK</h2>
-                <p className='text-center text-1xl font-bold  text-white'>T.B.A</p>
-                <div className='flex justify-between mx-auto md:w-[50%] my-6 px-6 py-3'>
-                    <a href="https://www.instagram.com/fosscu/"> <FaInstagram size={30} color='white' /> </a>
-                    <a href="https://twitter.com/fosscuk"> <FaTwitterSquare size={30} color='white' /> </a>
-                    <a href="https://github.com/FOSS-Community/"> <FaGithubSquare size={30} color='white' /> </a>
+                <p className='text-center text-1xl font-bold  text-white'>Volunteer in Frontend Development</p>
+                <div className='flex justify-center mx-auto md:w-[50%] my-6 px-6 py-3'>
+                <a href=""> <FaLink  size={30} color='white'/> </a>
+                </div>
+            </div>
+            
+            <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                <img className='w-20 mx-auto mt-[-3rem] bg-white profile_pic' src={Yash_Varshney} alt="/" />
+                <h2 className='text-2xl font-bold text-center py-8 text-white'>YASH VARSHNEY</h2>
+                <p className='text-center text-1xl font-bold text-white'>Volunteer in Web Development</p>
+                <div className='flex justify-center mx-auto md:w-[50%] my-6 px-6 py-3'>
+                <a href=""> <FaLink  size={30} color='white'/> </a>
+                </div>
+            </div>
+            <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                <img className='w-20 mx-auto mt-[-3rem] bg-white profile_pic' src={Srijan_Srivastava} alt="/" />
+                <h2 className='text-2xl font-bold text-center py-8  text-white'>Srijan Srivastava</h2>
+                <p className='text-center text-1xl font-bold  text-white'>Volunteer in Frontend Development</p>
+                <div className='flex justify-center mx-auto md:w-[50%] my-6 px-6 py-3'>
+                <a href=""> <FaLink  size={30} color='white'/> </a>
+                </div>
+            </div>
+            <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                <img className='w-20 mx-auto mt-[-3rem] bg-white profile_pic' src={Devanshu_Saxena} alt="/" />
+                <h2 className='text-2xl font-bold text-center py-8  text-white'>Devanshu Saxena</h2>
+                <p className='text-center text-1xl font-bold  text-white'>Volunteer in Web Development</p>
+                <div className='flex justify-center mx-auto md:w-[50%] my-6 px-6 py-3'>
+                <a href=""> <FaLink  size={30} color='white'/> </a>
                 </div>
             </div>
         </div>
