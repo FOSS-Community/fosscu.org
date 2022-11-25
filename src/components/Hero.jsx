18fb6f25ae85d38180dc2df22b1b73b6b2bfe4d4
Akshat Sharma <akshatcoder@outlook.com>
2022-11-25 13:41:49 +0000

    Updated form link🥚

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ function Hero() {
                 loop
               />
             </div>
-            <a href="https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__kzFzfFUNFdWMTVWTjhSU1UyMDZTN0YzSkhMWVE5Vi4u" target="_blank" rel="noopener noreferrer">
+            <a href="https://airtable.com/shrf9IfcWeG39JfbD" target="_blank" rel="noopener noreferrer">
               <button className='bg-[#0DFF1C] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Join Us 🤝</button>
             </a>
           </div>
