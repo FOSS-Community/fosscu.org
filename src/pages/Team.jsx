df7a6a7ee1e305453a6463f106c99f6b6befa257
Akshat Sharma <akshatcoder@outlook.com>
2022-11-25 16:45:28 +0000

    Updated Volunteers 👥

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -55,14 +55,14 @@ const Team = () => {
                 </div>
             </div>
 
-            <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+            {/* <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
                 <img className='w-20 mx-auto mt-[-3rem] bg-white profile_pic' src={Aditya_Bansal} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8 text-white'>ADITYA BANSAL</h2>
                 <p className='text-center text-1xl font-bold text-white'>Volunteer in Social Media</p>
                 <div className='flex justify-center mx-auto md:w-[50%] my-6 px-6 py-3'>
                 <a href="https://linktr.ee/adityabansal2310 "> <FaLink  size={30} color='white'/> </a>
                 </div>
-            </div>
+            </div> */}
 
 
             
