0a28a5fbf60b7da20dcfa101fafd906e70ad2356
Mayank Agarwal <agarwalmayank1170@gmail.com>
2022-12-24 17:43:13 +0000

    added Telegram, Discord and Matrix Button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ import Lottie from "react-lottie";
 import animationData from "../assets/lotties/community_animation_lottie.json";
 import Typed from "react-typed";
 
+import Discord from '../assets/img/logo/discord.png';
 function Hero() {
   const [size, setSize] = useState(window.innerWidth);
 
@@ -32,16 +33,16 @@ function Hero() {
     <div className="w-full py-16 px-5 ">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
         {
-          size>=700
-          ?
+          size >= 700
+            ?
 
-        <Lottie
-          className="w-[500px] md:w-[10px] mx-auto my-4"
-          options={defaultOptions}
-          height={400}
-          width={400}
-        />
-        : ''
+            <Lottie
+              className="w-[500px] md:w-[10px] mx-auto my-4"
+              options={defaultOptions}
+              height={400}
+              width={400}
+            />
+            : ''
         }
         <div className="flex flex-col justify-center">
           <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
@@ -65,9 +66,23 @@ function Hero() {
                 loop
               />
             </div>
-            <a href="https://discord.gg/8RQYESyj" target="_blank" rel="noopener noreferrer">
-              <button className='bg-[#0DFF1C] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Join Us 🤝</button>
-            </a>
+            <div class="flex items-center justify-center mb-3 mt-6">
+              <a href="https://t.me/+5uxiLKV0DHQxNzk9" target="_blank" rel="noopener noreferrer">
+                <button type="button" className="inline-block mx-auto py-3 my-0 bg-[#0DFF1C] w-[200px] text-black font-medium  rounded ">Telegram 🤝</button>
+              </a>
+            </div>
+            <div class="flex items-center justify-center mb-3">
+              <a href="https://discord.gg/8RQYESyj" target="_blank" rel="noopener noreferrer">
+                <button type="button" className="flex items-center justify-center inline-block mx-auto py-3 my-0 bg-[#0DFF1C] w-[200px] text-black font-medium  rounded ">Discord <img className='w-8' src={Discord} /> </button>
+              </a>
+            </div>
+            <div class="flex items-center justify-center mb-3">
+              <a href="https://matrix.to/#/!NYOIsHjFcqTHgsBtcj:matrix.org?via=matrix.org" target="_blank" rel="noopener noreferrer">
+                <button type="button" className="inline-block mx-auto py-3 my-0 bg-[#0DFF1C] w-[200px] text-black font-medium  rounded ">Matrix 🤝</button>
+              </a>
+            </div>
+
+
           </div>
         </div>
       </div>
@@ -76,3 +91,7 @@ function Hero() {
 }
 
 export default Hero;
+
+
+
+
