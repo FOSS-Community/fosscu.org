542e17e5a185704f51b90d83782987b23a5ad4fe
Kanishk Pachauri <krishna03012003@icloud.com>
2023-02-17 22:23:06 +0530

    Fixed Prod compilation errors

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
-// require('dotenv').config()
+// import React, { useState } from "react";
+// // require('dotenv').config()
 
-import axios from "axios";
-import env from "react-dotenv";
-const Contact = () => {
- 
-  
-  return (
-    <section class="text-gray-400 bg-gray-1000 body-font relative">
-     
-    </section>
-  );
-};
-export default Contact;
+// import axios from "axios";
+// import env from "react-dotenv";
+// const Contact = () => {
+
+//   return (
+//     <section class="text-gray-400 bg-gray-1000 body-font relative">
+
+//     </section>
+//   );
+// };
+// export default Contact;
