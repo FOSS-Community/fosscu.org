1bcded2b803e8cd4a24003248469b898ba7714e9
Shivam KUmar <shivambharadwaj822@gmail.com>
2023-02-28 01:42:09 +0530

    Correctd the building spelling

--- src/components/Building.jsx
+++ src/components/Building.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Building = () => {
   return (
     <div className="w-full py-16 text-white px-4 text-center text-5xl ">
-      We are Buiding
+      We are Building
     </div>
   );
 };
