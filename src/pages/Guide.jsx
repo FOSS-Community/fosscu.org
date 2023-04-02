f96f860f3fcf2f3d7330b33aa62d90b79565f747
Alexander Alexeev <san73rus@gmail.com>
2023-04-02 22:05:40 +0300

    Guideline page added

---
+++ src/pages/Guide.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import GuideElement from "../components/Guide Element";
+
+const Guide = () => {
+    return (
+        <div>
+            <Navbar />
+            <GuideElement />
+            <Footer />
+        </div>
+    );
+};
+
+export default Guide;
