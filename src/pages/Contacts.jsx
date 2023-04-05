aa98e2a8a141b9ae52d60e9616051bc76401d7f7
Kanishk Pachauri <krishna03012003@icloud.com>
2023-04-06 04:59:41 +0530

    Code cleanup 🧹
    Just doing some code cleanup removing unused imports.

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import Contact from "../components/Contact";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import axios from "axios";
