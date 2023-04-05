b13ca2b82db47ff2a3b37f8d0f7648a4fbfa9e8c
Kanishk Pachauri <krishna03012003@icloud.com>
2023-04-05 19:15:51 +0000

    Added details of a member and do some changes 🥳

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,3 @@
-// import React from "react";
 import React, { useState, useEffect } from "react";
 import Lottie from "react-lottie";
 import animationData from "../assets/lotties/community_animation_lottie.json";
