0d0eed22e39918be6691ef74764ce8230e8508df
codecShivam <codecshivam@gmail.com>
2023-04-23 19:22:48 +0530

    build resources

---
+++ src/pages/Resources.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Resource from "../components/Resource"
+const Resources = () => {
+    return (
+        <div>
+            <Navbar />
+            <Resource />
+            <Footer />
+        </div>
+    );
+};
+
+export default Resources;
