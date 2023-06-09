6b4ca9fc9944fd59e0066820a57a63c63f9e843d
hiCXK <hicsk24@gmail.com>
2023-06-09 23:28:18 +0530

    added files and links

---
+++ src/components/PrivacyPolicy.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="w-full py-16 text-white px-4 text-center text-5xl ">
+      We are WRITING
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
