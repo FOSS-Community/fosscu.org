bd4292b3abb427a5dd819eb71f92632f007d6f1c
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-08-28 19:37:49 +0000

    feat: Add new 404 page with some eastereggs 🐣

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import Navbar from "./components/Navbar";
 import Team from "./pages/Team";
 import Contacts from "./pages/Contacts";
-import Shipyard from "./pages/Shipyard"
+import Shipyard from "./pages/Shipyard";
 import About from "./pages/AboutOrg";
 import Buiding from "./pages/Buiding";
-import ReactDOM from "react-dom/client";
 import Guide from "./pages/Guide";
 import Faqs from "./pages/Faqs";
 import Resources from "./pages/Resources";
@@ -21,51 +20,37 @@ import Tail_wind from "./pages/Frontend/Tail_wind";
 import Vue__js from "./pages/Frontend/Vue__js";
 import React__native from "./pages/Frontend/React__native";
 import Three__js from "./pages/Frontend/Three__js";
-
-// import About from './components/About'
-// import AboutOrg from "./pages/AboutOrg";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/home" element={<Homepage />}></Route>
-        <Route path="/team" element={<Team />}></Route>
-        <Route path="/contact" element={<Contacts />}></Route>
-        <Route path="/shipyard" element={<Shipyard />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/building" element={<Buiding />}></Route>
-        <Route path="/guide" element={<Guide />}></Route>
-        <Route path="/faq" element={<Faqs />}></Route>
-        <Route path="/resource" element={<Resources />}></Route>
-        <Route path="/privacypolicy" element={<Privacy />}></Route>
-        <Route path="/frontend" element={<Frontends />}></Route>
-        <Route path="/html_css" element={<HTMLCSS />}></Route>
-        <Route path="/javascript" element={<JS />}></Route>
-        <Route path="/electron" element={<Eletron />}></Route>
-        <Route path="/reactjs" element={<Reactjs />}></Route>
-        <Route path="/nextjs" element={<Next__js />}></Route>
-        <Route path="/tailwind" element={<Tail_wind />}></Route>
-        <Route path="/vuejs" element={<Vue__js />}></Route>
-        <Route path="/threejs" element={<Three__js />}></Route>
-        <Route path="/reactnative" element={<React__native />}></Route>
-        <Route path="*" element={<PageNotFound />}></Route>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/contact" element={<Contacts />} />
+        <Route path="/shipyard" element={<Shipyard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/building" element={<Buiding />} />
+        <Route path="/guide" element={<Guide />} />
+        <Route path="/faq" element={<Faqs />} />
+        <Route path="/resource" element={<Resources />} />
+        <Route path="/privacypolicy" element={<Privacy />} />
+        <Route path="/frontend" element={<Frontends />} />
+        <Route path="/html_css" element={<HTMLCSS />} />
+        <Route path="/javascript" element={<JS />} />
+        <Route path="/electron" element={<Eletron />} />
+        <Route path="/reactjs" element={<Reactjs />} />
+        <Route path="/nextjs" element={<Next__js />} />
+        <Route path="/tailwind" element={<Tail_wind />} />
+        <Route path="/vuejs" element={<Vue__js />} />
+        <Route path="/threejs" element={<Three__js />} />
+        <Route path="/reactnative" element={<React__native />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
-{
-  /* <Navbar />
-<Hero />
-<About />
-<Newsletter/> */
-}
