84d523b0edaa6628e415136c45371d22c5df3ad5
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-12-28 04:27:00 +0530

    added manifest.json and other icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,32 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "FOSSCU",
-  description: "Free and Open Source Software Community United",
+  description: "We at FOSSCU (Free and Open-Source Software Community United) are a Group of individuals who are constantly working to promote Open-Source Culture",
+  icons: {
+    icon: "/fosscu.png",
+    apple: "/logo192.png",
+  },
+  manifest: "/manifest.json",
+  themeColor: "#000000",
+  openGraph: {
+    title: "FOSSCU",
+    description: "We at FOSSCU (Free and Open-Source Software Community United) are a Group of individuals who are constantly working to promote Open-Source Culture",
+    url: "https://fosscu.org",
+    siteName: "FOSSCU",
+    images: [
+      {
+        url: "https://fosscu.org/fosscu.png",
+      },
+    ],
+    type: "website",
+  },
+  other: {
+    "google-site-verification": "your-verification-code",
+    "itemprop:name": "We ❤️ Open Source",
+    "itemprop:description": "We at FOSSCU (Free and Open-Source Software Community United) are a Group of individuals who are constantly working to promote Open-Source Culture",
+    "itemprop:image": "https://raw.githubusercontent.com/FOSS-Community/website-fossc/7fb82481a6d40ca5ceacbdeac46da76f9932a31c/src/assets/img/logo/fosscu.png",
+  },
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
