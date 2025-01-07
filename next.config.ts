c1ede2516c0bab9682d7374034a3d6c21d335cfd
Utkarsh4517 <uttkarsh.dav@gmail.com>
2025-01-07 06:13:15 +0530

    added redirect links in next config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,65 @@ const nextConfig: NextConfig = {
       'res.cloudinary.com',
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/hJ2utJX2j5',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/fosscuk',
+        permanent: true,
+      },
+      {
+        source: '/x',
+        destination: 'https://twitter.com/fosscuk',
+        permanent: true,
+      },
+      {
+        source: '/insta',
+        destination: 'https://instagram.com/fosscu',
+        permanent: true,
+      },
+      {
+        source: '/instagram',
+        destination: 'https://instagram.com/fosscu',
+        permanent: true,
+      },
+      {
+        source: '/pastebin',
+        destination: 'https://paste.fosscu.org/',
+        permanent: true,
+      },
+      {
+        source: '/fosscu-chapters',
+        destination: 'https://airtable.com/appFI181bKZEl3uMz/pagsGmcR2dGne7qlJ/form',
+        permanent: true,
+      },
+      {
+        source: '/non-tech-fosscu',
+        destination: 'https://airtable.com/appIZUCp0jbQIfGjy/pag283Ew6mnfIvLr7/form',
+        permanent: true,
+      },
+      {
+        source: '/wand/install.sh',
+        destination: 'https://raw.githubusercontent.com/FOSS-Community/wand/refs/heads/main/install.sh',
+        permanent: true,
+      },
+      {
+        source: '/tps',
+        destination: 'https://github.com/Mr-Sunglasses/the-python-show',
+        permanent: true,
+      },
+      {
+        source: '/tps/feedback',
+        destination: 'https://forms.gle/ZUEqDfacL6GAwubw9',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
